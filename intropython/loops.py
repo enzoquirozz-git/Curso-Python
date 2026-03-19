e0d06fa7f58d54db7d4b17457e35fa6037977ba1
Enzo Eduardo Quiroz <enzoquiroz2912@ca.frre.utn.edu.ar>
2026-03-19 11:01:33 -0300

    Add files via upload

---
+++ intropython/loops.py
@@ -0,0 +1,14 @@
+#i = 0
+
+#while i < 5:
+ #   print (i)
+  #  if i == 3:
+   #     break
+
+    #i += 1
+
+usuarios = ['stuart', "mafalda", "enzo", "lola", "maggie"]
+
+for usuario in usuarios:
+    print(usuario)
+    
