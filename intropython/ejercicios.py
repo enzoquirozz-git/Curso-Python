2f53d5c7fc2021bd3088c79cd82a95c2c5f597ac
Enzo Eduardo Quiroz <enzoquiroz2912@ca.frre.utn.edu.ar>
2026-03-19 11:06:14 -0300

    Delete intropython directory

--- intropython/ejercicios.py
+++
@@ -1,8 +0,0 @@
-# dato = input("Ingrese un dato: ")
-
-# lista = ['perro', 'gato', 'coballo', 'chanchito', 'vaca']
-# if lista.count(dato) > 0:
-#     print("El dato", dato, "se encuentra en la lista")
-# else:
-#     print("El dato", dato, "no se encuentra en la lista")
-    
